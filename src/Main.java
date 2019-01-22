40c1833632d5a7138977b21e7252c46123bb3be2
Maczi01 <system11>
2019-01-23 00:57:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Room room = new Room();
+        System.out.println("Jaka jest powierzchnia pokoju? [m2]: ");
+        int surface = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Jaka jest temperatura w pokoju?: ");
+        int temperature = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Czy w pokoju jest klimatyzator ?: ");
+        boolean ac = scanner.nextBoolean();
+
+        room.setSurface(surface);
+        room.setActualTemperature(temperature);
+        room.setAirConditioner(ac);
+
+        room.showInfo(room);
+        room.colder(room);
+        room.showInfo(room);
+        room.colder(room);
+        room.showInfo(room);
+        room.colder(room);
+        room.showInfo(room);
+        room.colder(room);
+        room.showInfo(room);
+
+
+
+    }
+}
---
+++ src/Room.java
@@ -0,0 +1,64 @@
+public class Room {
+
+    private final int minimalTemperature = 15;
+    private int surface;
+    private int actualTemperature;
+    private boolean AirConditioner;
+
+    boolean colder(Room room) {
+        boolean isWork = false;
+        if(room.isAirConditioner()){
+            room.setActualTemperature(getActualTemperature()-1);
+            return true;
+        }
+        return false;
+    }
+
+    void showInfo(Room room){
+        System.out.println("Temperature: " + room.getActualTemperature());
+        System.out.println("Room surface: " +room.getSurface());
+        System.out.println("Room has conditioner: " + room.isAirConditioner());
+        System.out.println("-------------------------");
+    }
+
+    public Room() {
+    }
+
+    public Room(int surface, int actualTemperature, boolean AirConditioner) {
+        this.surface = surface;
+        this.actualTemperature = actualTemperature;
+        this.AirConditioner = AirConditioner;
+    }
+
+    public int getMinimalTemperature() {
+        return minimalTemperature;
+    }
+
+    public int getSurface() {
+        return surface;
+    }
+
+    public void setSurface(int surface) {
+        this.surface = surface;
+    }
+
+    public int getActualTemperature() {
+        return actualTemperature;
+    }
+
+    public void setActualTemperature(int actualTemperature) {
+        if (actualTemperature < minimalTemperature) {
+            System.out.println("Zbyt niska temperatura !!!");
+        } else {
+            this.actualTemperature = actualTemperature;
+        }
+    }
+
+    public boolean isAirConditioner() {
+        return AirConditioner;
+    }
+
+    public void setAirConditioner(boolean airConditioner) {
+        AirConditioner = airConditioner;
+    }
+}
